4aa13e4a168fbeeac90245525d93b1c2321fbd24
KhDan007 <kh.dan007@gmail.com>
2023-05-05 13:32:47 +0600

    solved some tasks

---
+++ 1.JavaSyntax/src/en/javarush/task/jdk13/task09/task0915/Solution.java
@@ -0,0 +1,58 @@
+package en.javarush.task.jdk13.task09.task0915;
+
+import java.io.CharConversionException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+
+/* 
+Catching custom exceptions
+*/
+
+public class Solution {
+    public static StatelessBean BEAN = new StatelessBean();
+
+    public static void main(String[] args){
+        try {
+            handleExceptions();
+        }
+        catch (FileSystemException e) {
+            BEAN.log(e);
+        }
+    }
+
+    public static void handleExceptions() throws FileSystemException {
+        try {
+            BEAN.throwExceptions();
+        }
+        catch (FileSystemException e) {
+            BEAN.log(e);
+            throw e;
+        }
+        catch (CharConversionException e) {
+            BEAN.log(e);
+        }
+        catch (IOException e) {
+            BEAN.log(e);
+        }
+    }
+
+    public static class StatelessBean {
+
+        public int i = (int) (Math.random() * 3);
+
+        public void log(Exception exception) {
+            System.out.println(exception.getMessage() + ", " + exception.getClass().getSimpleName());
+        }
+
+        public void throwExceptions() throws CharConversionException, FileSystemException, IOException {
+            if (i == 0) {
+                throw new CharConversionException();
+            } else if (i == 1) {
+                throw new FileSystemException("");
+            } else if (i == 2) {
+                throw new IOException();
+            }
+        }
+    }
+}
