8a656a1e84cd751408999d43053ac145822fbee4
KhDan007 <kh.dan007@gmail.com>
2023-05-06 15:47:08 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a){
+
+    }
+    public static void print(Integer a) {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+
+    }
+    public static void print(int a, int b) {
+
+    }
+    public static void print(int a, String name) {
+
+    }
+    public static void print(int a, boolean condition) {
+
+    }
+    public static void print(int a, double b) {
+
+    }
+}
