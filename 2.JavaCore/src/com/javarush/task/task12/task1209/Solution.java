d5268deaea10e008aa48be9c2da78fae65cb4215
KhDan007 <kh.dan007@gmail.com>
2023-05-06 15:48:16 +0600

    solved some tasks

--- 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -12,9 +12,11 @@ public class Solution {
     public static int min(int a, int b) {
         return Math.min(a, b);
     }
+
     public static double min(double a, double b) {
         return Math.min(a, b);
     }
+
     public static long min(long a, long b) {
         return Math.min(a, b);
     }
