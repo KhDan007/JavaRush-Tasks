1a9ad1300620da001e13974bbb4ae6b0ad5a5559
KhDan007 <kh.dan007@gmail.com>
2023-05-06 13:37:31 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1106/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task11.task1106;
+
+/* 
+Скрытный инкапсулированный кот
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat() {
+        }
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+    }
+}
+
