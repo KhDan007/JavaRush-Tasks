ed37c356c9f79f9e49125550100dab92ac67a309
KhDan007 <kh.dan007@gmail.com>
2023-05-08 22:17:49 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task15.task1529;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Осваивание статического блока
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static {
+        try {
+            reset();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static CanFly result;
+
+    public static void reset() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        if (line.equals("helicopter")) {
+            result = new Helicopter();
+        } else if (line.equals("plane")) {
+            result = new Plane(10);
+        }
+        reader.close();
+    }
+}
