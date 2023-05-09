88adc9ee644887981f6a54837b6e1d97af8889c3
KhDan007 <kh.dan007@gmail.com>
2023-05-09 11:40:07 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    private Solution(int n){}
+    Solution(){}
+    protected Solution(String s){}
+    public Solution(double d){}
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution() {
+    }
+
+    protected SubSolution(String s) {
+        super(s);
+    }
+
+    public SubSolution(double d) {
+        super(d);
+    }
+}
