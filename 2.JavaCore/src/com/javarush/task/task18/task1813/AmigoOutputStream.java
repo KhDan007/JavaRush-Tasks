fd6101eec8ac5f3cd02ca45d521a0055d492688b
KhDan007 <kh.dan007@gmail.com>
2023-05-13 11:06:41 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task18.task1813;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+AmigoOutputStream
+*/
+
+public class AmigoOutputStream extends FileOutputStream {
+    public static String fileName = "C:/tmp/result.txt";
+    private FileOutputStream original;
+
+    public AmigoOutputStream(FileOutputStream original) throws FileNotFoundException {
+        super(fileName);
+        this.original = original;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new AmigoOutputStream(new FileOutputStream(fileName));
+    }
+
+    @Override
+    public void close() throws IOException {
+        original.flush();
+        original.write("JavaRush © All rights reserved.".getBytes());
+        original.close();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        original.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        original.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        original.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        original.flush();
+    }
+}
