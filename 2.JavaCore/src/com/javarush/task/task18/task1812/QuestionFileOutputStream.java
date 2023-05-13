2333926762414ea8e941af2fe36f7450654bed60
KhDan007 <kh.dan007@gmail.com>
2023-05-13 10:53:00 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task18.task1812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Расширяем AmigoOutputStream
+*/
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+    private AmigoOutputStream original;
+
+    public QuestionFileOutputStream(AmigoOutputStream original) {
+        this.original = original;
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        if (line.equals("Д")) {
+            original.close();
+        }
+    }
+
+    @Override
+    public void flush() throws IOException {
+        original.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        original.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        original.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        original.write(b, off, len);
+    }
+}
+
