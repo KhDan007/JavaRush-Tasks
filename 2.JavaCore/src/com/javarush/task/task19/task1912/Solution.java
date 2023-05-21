a6db6cbee93cf93c67c783067a1082e53ba7cd4c
KhDan007 <kh.dan007@gmail.com>
2023-05-21 16:28:48 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task19.task1912;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка 2
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream myStream = new PrintStream(outputStream);
+        System.setOut(myStream);
+
+        testString.printSomething();
+        System.setOut(consoleStream);
+
+        String result = outputStream.toString().replaceAll("te", "??");
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
