ada6efb6caa9fdac697ed46050d79824e47952d9
KhDan007 <kh.dan007@gmail.com>
2023-07-05 10:54:55 +0600

    solved some tasks

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task20.task2022;
+
+import java.io.*;
+
+/* 
+Переопределение сериализации в потоке
+*/
+
+public class Solution implements Serializable, AutoCloseable {
+    private transient FileOutputStream stream;
+    private String fileName;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws Exception {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws Exception {
+        in.defaultReadObject();
+        this.stream = new FileOutputStream(fileName, true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Solution sol = new Solution("C:\\Users\\dan\\Desktop\\test.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(sol.stream);
+        oos.writeObject("some random text");
+
+        oos.writeObject(sol);
+
+        String loadedObject = (String) new ObjectInputStream(new FileInputStream("C:\\Users\\dan\\Desktop\\test.txt")).readObject();
+        System.out.println(loadedObject);
+    }
+}
