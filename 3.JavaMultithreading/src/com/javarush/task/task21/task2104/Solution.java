cfc74c8e5d6d33c5b6f6bbf8824acf9f53a7436d
KhDan007 <kh.dan007@gmail.com>
2023-07-11 06:36:37 +0600

    solved some tasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object n) {
+        if (this == n) return true;
+        if (!(n instanceof Solution)) return false;
+        Solution s = (Solution) n;
+
+        if (!Objects.equals(first, s.first)) return false;
+        return Objects.equals(last, s.last);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
