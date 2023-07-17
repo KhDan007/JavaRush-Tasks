abf2dbec43f488b4a9b6f478e57955d9fa9a7f58
KhDan007 <kh.dan007@gmail.com>
2023-07-17 09:54:04 +0600

    solved some tasks

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,4 +1,44 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public Snake(int x, int y){
+        this.sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+
+    public void move() {
+
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
 }
